8d6d2191ea97adcc10f0351745d666b2e966d528
Anna Kusova <kusovanna@gmail.com>
2026-01-26 22:16:45 +0200

    create website of research pic

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const query = event.target.elements['search-text'].value.trim();
+  if (!query) {
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+  try {
+    const data = await getImagesByQuery(query);
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({
+      message: 'Something went wrong. Please try again later!',
+    });
+  } finally {
+    hideLoader();
+  }
+});
